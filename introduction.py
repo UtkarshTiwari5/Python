a632ac69a5ec60a0b6c193b4cd10a6c1b65b0ad4
UtkarshTiwari5 <utkarshtiwari1772@gmail.com>
2025-01-12 15:41:13 +0530

    Add files via upload
    program toi introduction in python

---
+++ introduction.py
@@ -0,0 +1,49 @@
+# print("harshit")
+# name = 23
+# print(name)
+
+# age = "utkarsh"
+# print(age)
+
+# print("age")
+
+# A = "utkarsh"
+
+# _utkarsh= "sdjkncj"
+# print(_utkarsh)
+
+age = (6 , 6 , 7) 
+print(age)
+name = "pankaj"
+a = False
+b = 1.5
+print(type(name))
+print(type(a))
+print(type(age))
+print(type(b))
+
+list = [2,3]
+list2 =["utkarsh,pankaj,"]
+list3 =(6,7,8)
+list4 = { 1,3,4}
+dis={"age":12,
+    "name":"pankaj"}
+    
+
+print(type(list))
+print(type(list2))
+print(type(list3))
+print(type(list4))
+print(type(dis))
+
+
+print("list")
+print(list2)
+print(list3)
+print(list4)
+print(dis)
+
+
+
+
+
